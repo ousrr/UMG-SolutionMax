989dd166e7a6c0ce96bf1de3c4639f6420ed056e
ousrr <kramirezr17@miumg.edu.gt>
2026-03-19 17:51:55 -0600

    Fix: frontend agregado correctamente

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import EstadoCliente from "./pages/EstadoCliente";
+
+function App() {
+  return (
+    <div>
+      <h1>Solution Max</h1>
+      <EstadoCliente />
+    </div>
+  );
+}
+
+export default App;
